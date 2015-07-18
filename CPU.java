254ad9555b0d9e4632ad8bfcab01a740e19d66a4
Tad Parrish <tad.parrish@yahoo.com>
2015-07-17 21:36:01 -0700

    Added several Opcodes that need testing.

--- CPU.java
+++ CPU.java
@@ -55,11 +55,224 @@ public class CPU
 			case 0x06:
 				cyclesUsed = Instruction.ld_B_n();
 			break;
+			case 0x0E:
+				cyclesUsed = Instruction.ld_C_n();
+			break;
+			case 0x16:
+				cyclesUsed = Instruction.ld_D_n();
+			break;
+			case 0x1E:
+				cyclesUsed = Instruction.ld_E_n();
+			break;
+			case 0x26:
+				cyclesUsed = Instruction.ld_H_n();
+			break;
+			case 0x2E:
+				cyclesUsed = Instruction.ld_L_n();
+			break;
+			case 0x36:
+				cyclesUsed = Instruction.ld_$HL_n();
+			break;
+			case 0x40:
+				cyclesUsed = Instruction.ld_B_B();
+			break;
+			case 0x41:
+				cyclesUsed = Instruction.ld_B_C();
+			break;
+			case 0x42:
+				cyclesUsed = Instruction.ld_B_D();
+			break;
+			case 0x43:
+				cyclesUsed = Instruction.ld_B_E();
+			break;
+			case 0x44:
+				cyclesUsed = Instruction.ld_B_H();
+			break;
+			case 0x45:
+				cyclesUsed = Instruction.ld_B_L();
+			break;
+			case 0x46:
+				cyclesUsed = Instruction.ld_B_$HL();
+			break;
+			case 0x48:
+				cyclesUsed = Instruction.ld_C_B();
+			break;
+			case 0x49:
+				cyclesUsed = Instruction.ld_C_C();
+			break;
+			case 0x4A:
+				cyclesUsed = Instruction.ld_C_D();
+			break;
+			case 0x4B:
+				cyclesUsed = Instruction.ld_C_E();
+			break;
+			case 0x4C:
+				cyclesUsed = Instruction.ld_C_H();
+			break;
+			case 0x4D:
+				cyclesUsed = Instruction.ld_C_L();
+			break;
+			case 0x4E:
+				cyclesUsed = Instruction.ld_C_$HL();
+			break;
+			case 0x50:
+				cyclesUsed = Instruction.ld_D_B();
+			break;
+			case 0x51:
+				cyclesUsed = Instruction.ld_D_C();
+			break;
+			case 0x52:
+				cyclesUsed = Instruction.ld_D_D();
+			break;
+			case 0x53:
+				cyclesUsed = Instruction.ld_D_E();
+			break;
+			case 0x54:
+				cyclesUsed = Instruction.ld_D_H();
+			break;
+			case 0x55:
+				cyclesUsed = Instruction.ld_D_L();
+			break;
+			case 0x56:
+				cyclesUsed = Instruction.ld_D_$HL();
+			break;
+			case 0x58:
+				cyclesUsed = Instruction.ld_E_B();
+			break;
+			case 0x59:
+				cyclesUsed = Instruction.ld_E_C();
+			break;
+			case 0x5A:
+				cyclesUsed = Instruction.ld_E_D();
+			break;
+			case 0x5B:
+				cyclesUsed = Instruction.ld_E_E();
+			break;
+			case 0x5C:
+				cyclesUsed = Instruction.ld_E_H();
+			break;
+			case 0x5D:
+				cyclesUsed = Instruction.ld_E_L();
+			break;
+			case 0x5E:
+				cyclesUsed = Instruction.ld_E_$HL();
+			break;
+			case 0x60:
+				cyclesUsed = Instruction.ld_H_B();
+			break;
+			case 0x61:
+				cyclesUsed = Instruction.ld_H_C();
+			break;
+			case 0x62:
+				cyclesUsed = Instruction.ld_H_D();
+			break;
+			case 0x63:
+				cyclesUsed = Instruction.ld_H_E();
+			break;
+			case 0x64:
+				cyclesUsed = Instruction.ld_H_H();
+			break;
+			case 0x65:
+				cyclesUsed = Instruction.ld_H_L();
+			break;
+			case 0x66:
+				cyclesUsed = Instruction.ld_H_$HL();
+			break;
+			case 0x68:
+				cyclesUsed = Instruction.ld_L_B();
+			break;
+			case 0x69:
+				cyclesUsed = Instruction.ld_L_C();
+			break;
+			case 0x6A:
+				cyclesUsed = Instruction.ld_L_D();
+			break;
+			case 0x6B:
+				cyclesUsed = Instruction.ld_L_E();
+			break;
+			case 0x6C:
+				cyclesUsed = Instruction.ld_L_H();
+			break;
+			case 0x6D:
+				cyclesUsed = Instruction.ld_L_L();
+			break;
+			case 0x6E:
+				cyclesUsed = Instruction.ld_L_$HL();
+			break;
+			case 0x70:
+				cyclesUsed = Instruction.ld_$HL_B();
+			break;
+			case 0x71:
+				cyclesUsed = Instruction.ld_$HL_C();
+			break;
+			case 0x72:
+				cyclesUsed = Instruction.ld_$HL_D();
+			break;
+			case 0x73:
+				cyclesUsed = Instruction.ld_$HL_E();
+			break;
+			case 0x74:
+				cyclesUsed = Instruction.ld_$HL_H();
+			break;
+			case 0x75:
+				cyclesUsed = Instruction.ld_$HL_L();
+			break;
+			case 0x78:
+				cyclesUsed = Instruction.ld_A_B();
+			break;
+			case 0x79:
+				cyclesUsed = Instruction.ld_A_C();
+			break;
+			case 0x7A:
+				cyclesUsed = Instruction.ld_A_D();
+			break;
+			case 0x7B:
+				cyclesUsed = Instruction.ld_A_E();
+			break;
+			case 0x7C:
+				cyclesUsed = Instruction.ld_A_H();
+			break;
+			case 0x7D:
+				cyclesUsed = Instruction.ld_A_L();
+			break;
+			case 0x7E:
+				cyclesUsed = Instruction.ld_A_$HL();
+			break;
+			case 0x7F:
+				cyclesUsed = Instruction.ld_A_A();
+			break;
 			case 0x87:
 				cyclesUsed = Instruction.add_A_A();
 			break;
+			case 0xA8:
+				cyclesUsed = Instruction.xor_B();
+			break;
+			case 0xA9:
+				cyclesUsed = Instruction.xor_C();
+			break;
+			case 0xAA:
+				cyclesUsed = Instruction.xor_D();
+			break;
+			case 0xAB:
+				cyclesUsed = Instruction.xor_E();
+			break;
+			case 0xAC:
+				cyclesUsed = Instruction.xor_H();
+			break;
+			case 0xAD:
+				cyclesUsed = Instruction.xor_L();
+			break;
+			case 0xAE:
+				cyclesUsed = Instruction.xor_$HL();
+			break;
+			case 0xAF:
+				cyclesUsed = Instruction.xor_A();
+			break;
 			case 0xC3:
 				cyclesUsed = Instruction.jp_nn();
+			break;
+			case 0xEE:
+				cyclesUsed = Instruction.xor_n();
 			break;
 		}
 
--- Instruction.java
+++ Instruction.java
@@ -4,6 +4,9 @@ method names of format:
 Add B to A: asm: add a, b
 add_A_B(); //returns number of cycles / 4
 
+ld A, (HL)
+ld_A_$HL();
+
 */
 
 /*
@@ -14,7 +17,7 @@ What needs to happen:
 */
 public class Instruction
 {
-	//Section 3.3.1 8-bit Loads
+	//Section 3.3.1-1 8-bit Loads
 	public static int ld_B_n() //opcode: 0x06, 0xnn
 	{
 		int data;
@@ -35,7 +38,795 @@ public class Instruction
 		return 2;
 	}
 
-	//Section 3.3.3 8-bit ALU
+	public static int ld_C_n() //opcode: 0x0E, 0xnn
+	{
+		int data;
+		int address;
+		//get address of immediate byte
+		Register.incPC(1);
+		address = Register.readPC();
+
+		//get immediate byte
+		data = Memory.read(address);
+
+		Register.writeC(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 2;
+	}
+
+	public static int ld_D_n() //opcode: 0x16, 0xnn
+	{
+		int data;
+		int address;
+		//get address of immediate byte
+		Register.incPC(1);
+		address = Register.readPC();
+
+		//get immediate byte
+		data = Memory.read(address);
+
+		Register.writeD(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 2;
+	}
+
+	public static int ld_E_n() //opcode: 0x1E, 0xnn
+	{
+		int data;
+		int address;
+		//get address of immediate byte
+		Register.incPC(1);
+		address = Register.readPC();
+
+		//get immediate byte
+		data = Memory.read(address);
+
+		Register.writeE(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 2;
+	}
+
+	public static int ld_H_n() //opcode: 0x26, 0xnn
+	{
+		int data;
+		int address;
+		//get address of immediate byte
+		Register.incPC(1);
+		address = Register.readPC();
+
+		//get immediate byte
+		data = Memory.read(address);
+
+		Register.writeH(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 2;
+	}
+
+	public static int ld_L_n() //opcode: 0x2E, 0xnn
+	{
+		int data;
+		int address;
+		//get address of immediate byte
+		Register.incPC(1);
+		address = Register.readPC();
+
+		//get immediate byte
+		data = Memory.read(address);
+
+		Register.writeL(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 2;
+	}
+
+	//Section 3.3.1-2 8-bit Loads
+	public static int ld_A_A() //opcode: 7F
+	{
+		int data = Register.readA();
+		Register.writeA(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_A_B() //opcode: 78
+	{
+		int data = Register.readB();
+		Register.writeA(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_A_C() //opcode: 79
+	{
+		int data = Register.readC();
+		Register.writeA(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_A_D() //opcode: 7A
+	{
+		int data = Register.readD();
+		Register.writeA(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_A_E() //opcode: 7B
+	{
+		int data = Register.readE();
+		Register.writeA(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_A_H() //opcode: 7C
+	{
+		int data = Register.readH();
+		Register.writeA(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_A_L() //opcode: 7D
+	{
+		int data = Register.readL();
+		Register.writeA(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_A_$HL() //opcode: 7E
+	{
+		int data = Memory.read(Register.readHL());
+		Register.writeA(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 2;
+	}
+
+	public static int ld_B_B() //opcode: 40
+	{
+		int data = Register.readB();
+		Register.writeB(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_B_C() //opcode: 41
+	{
+		int data = Register.readC();
+		Register.writeB(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_B_D() //opcode: 42
+	{
+		int data = Register.readD();
+		Register.writeB(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_B_E() //opcode: 43
+	{
+		int data = Register.readE();
+		Register.writeB(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_B_H() //opcode: 44
+	{
+		int data = Register.readH();
+		Register.writeB(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_B_L() //opcode: 45
+	{
+		int data = Register.readL();
+		Register.writeB(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_B_$HL() //opcode: 46
+	{
+		int data = Memory.read(Register.readHL());
+		Register.writeB(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 2;
+	}
+
+	public static int ld_C_B() //opcode: 48
+	{
+		int data = Register.readB();
+		Register.writeC(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_C_C() //opcode: 49
+	{
+		int data = Register.readC();
+		Register.writeC(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_C_D() //opcode: 4A
+	{
+		int data = Register.readD();
+		Register.writeC(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_C_E() //opcode: 4B
+	{
+		int data = Register.readE();
+		Register.writeC(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_C_H() //opcode: 4C
+	{
+		int data = Register.readH();
+		Register.writeC(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_C_L() //opcode: 4D
+	{
+		int data = Register.readL();
+		Register.writeC(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_C_$HL() //opcode: 4E
+	{
+		int data = Memory.read(Register.readHL());
+		Register.writeC(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 2;
+	}
+
+	public static int ld_D_B() //opcode: 50
+	{
+		int data = Register.readB();
+		Register.writeD(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_D_C() //opcode: 51
+	{
+		int data = Register.readC();
+		Register.writeD(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_D_D() //opcode: 52
+	{
+		int data = Register.readD();
+		Register.writeD(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_D_E() //opcode: 53
+	{
+		int data = Register.readE();
+		Register.writeD(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_D_H() //opcode: 54
+	{
+		int data = Register.readH();
+		Register.writeD(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_D_L() //opcode: 55
+	{
+		int data = Register.readL();
+		Register.writeD(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_D_$HL() //opcode: 56
+	{
+		int data = Memory.read(Register.readHL());
+		Register.writeD(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 2;
+	}
+
+	public static int ld_E_B() //opcode: 58
+	{
+		int data = Register.readB();
+		Register.writeE(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_E_C() //opcode: 59
+	{
+		int data = Register.readC();
+		Register.writeE(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_E_D() //opcode: 5A
+	{
+		int data = Register.readD();
+		Register.writeE(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_E_E() //opcode: 5B
+	{
+		int data = Register.readE();
+		Register.writeE(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_E_H() //opcode: 5C
+	{
+		int data = Register.readH();
+		Register.writeE(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_E_L() //opcode: 5D
+	{
+		int data = Register.readL();
+		Register.writeE(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_E_$HL() //opcode: 5E
+	{
+		int data = Memory.read(Register.readHL());
+		Register.writeE(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 2;
+	}
+
+	public static int ld_H_B() //opcode: 60
+	{
+		int data = Register.readB();
+		Register.writeH(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_H_C() //opcode: 61
+	{
+		int data = Register.readC();
+		Register.writeH(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_H_D() //opcode: 62
+	{
+		int data = Register.readD();
+		Register.writeH(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_H_E() //opcode: 63
+	{
+		int data = Register.readE();
+		Register.writeH(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_H_H() //opcode: 64
+	{
+		int data = Register.readH();
+		Register.writeH(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_H_L() //opcode: 65
+	{
+		int data = Register.readL();
+		Register.writeH(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_H_$HL() //opcode: 66
+	{
+		int data = Memory.read(Register.readHL());
+		Register.writeH(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 2;
+	}
+
+	public static int ld_L_B() //opcode: 68
+	{
+		int data = Register.readB();
+		Register.writeL(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_L_C() //opcode: 69
+	{
+		int data = Register.readC();
+		Register.writeL(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_L_D() //opcode: 6A
+	{
+		int data = Register.readD();
+		Register.writeL(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_L_E() //opcode: 6B
+	{
+		int data = Register.readE();
+		Register.writeL(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_L_H() //opcode: 6C
+	{
+		int data = Register.readH();
+		Register.writeL(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_L_L() //opcode: 6D
+	{
+		int data = Register.readL();
+		Register.writeL(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 1;
+	}
+
+	public static int ld_L_$HL() //opcode: 6E
+	{
+		int data = Memory.read(Register.readHL());
+		Register.writeL(data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 2;
+	}
+
+	public static int ld_$HL_B() //opcode: 70
+	{
+		int data = Register.readB();
+		Memory.write(Register.readHL(), data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 2;
+	}
+
+	public static int ld_$HL_C() //opcode: 71
+	{
+		int data = Register.readC();
+		Memory.write(Register.readHL(), data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 2;
+	}
+
+	public static int ld_$HL_D() //opcode: 72
+	{
+		int data = Register.readD();
+		Memory.write(Register.readHL(), data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 2;
+	}
+
+	public static int ld_$HL_E() //opcode: 73
+	{
+		int data = Register.readE();
+		Memory.write(Register.readHL(), data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 2;
+	}
+
+	public static int ld_$HL_H() //opcode: 74
+	{
+		int data = Register.readH();
+		Memory.write(Register.readHL(), data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 2;
+	}
+
+	public static int ld_$HL_L() //opcode: 75
+	{
+		int data = Register.readL();
+		Memory.write(Register.readHL(), data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 2;
+	}
+
+	public static int ld_$HL_n() //opcode: 36
+	{
+		//get address of immediate byte
+		Register.incPC(1);
+		int data = Memory.read(Register.readPC());
+		Memory.write(Register.readHL(), data);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return instruction time
+		return 3;
+	}
+
+
+	//Section 3.3.3-1 8-bit ALU
 	public static int add_A_A() //opcode: 0x87
 	{
 		int a = Register.readA();
@@ -79,6 +870,188 @@ public class Instruction
 
 		//return how many cycles / 4 instruction took.
 		return 1;
+	}
+
+	//Section 3.3.3-1 8-bit ALU
+	public static int xor_A() //Opcode: 0xAF
+	{
+		int data = Register.readA();
+		int temp = data ^ data;
+		Register.writeA(temp);
+		if(temp == 0) Register.setZeroFlag(true);
+		else Register.setZeroFlag(false);
+
+		Register.setSubtFlag(false);
+		Register.setHCarryFlag(false);
+		Register.setCarryFlag(false);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return how many cycles / 4 instruction took.
+		return 1;
+	}
+
+	public static int xor_B() //Opcode: 0xA8
+	{
+		int a = Register.readA();
+		int b = Register.readB();
+		int temp = a ^ b;
+		Register.writeA(temp);
+		if(temp == 0) Register.setZeroFlag(true);
+		else Register.setZeroFlag(false);
+
+		Register.setSubtFlag(false);
+		Register.setHCarryFlag(false);
+		Register.setCarryFlag(false);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return how many cycles / 4 instruction took.
+		return 1;
+	}
+
+	public static int xor_C() //Opcode: 0xA9
+	{
+		int a = Register.readA();
+		int b = Register.readC();
+		int temp = a ^ b;
+		Register.writeA(temp);
+		if(temp == 0) Register.setZeroFlag(true);
+		else Register.setZeroFlag(false);
+
+		Register.setSubtFlag(false);
+		Register.setHCarryFlag(false);
+		Register.setCarryFlag(false);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return how many cycles / 4 instruction took.
+		return 1;
+	}
+
+	public static int xor_D() //Opcode: 0xAA
+	{
+		int a = Register.readA();
+		int b = Register.readD();
+		int temp = a ^ b;
+		Register.writeA(temp);
+		if(temp == 0) Register.setZeroFlag(true);
+		else Register.setZeroFlag(false);
+
+		Register.setSubtFlag(false);
+		Register.setHCarryFlag(false);
+		Register.setCarryFlag(false);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return how many cycles / 4 instruction took.
+		return 1;
+	}
+
+	public static int xor_E() //Opcode: 0xAB
+	{
+		int a = Register.readA();
+		int b = Register.readE();
+		int temp = a ^ b;
+		Register.writeA(temp);
+		if(temp == 0) Register.setZeroFlag(true);
+		else Register.setZeroFlag(false);
+
+		Register.setSubtFlag(false);
+		Register.setHCarryFlag(false);
+		Register.setCarryFlag(false);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return how many cycles / 4 instruction took.
+		return 1;
+	}
+
+	public static int xor_H() //Opcode: 0xAC
+	{
+		int a = Register.readA();
+		int b = Register.readH();
+		int temp = a ^ b;
+		Register.writeA(temp);
+		if(temp == 0) Register.setZeroFlag(true);
+		else Register.setZeroFlag(false);
+
+		Register.setSubtFlag(false);
+		Register.setHCarryFlag(false);
+		Register.setCarryFlag(false);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return how many cycles / 4 instruction took.
+		return 1;
+	}
+
+	public static int xor_L() //Opcode: 0xAD
+	{
+		int a = Register.readA();
+		int b = Register.readL();
+		int temp = a ^ b;
+		Register.writeA(temp);
+		if(temp == 0) Register.setZeroFlag(true);
+		else Register.setZeroFlag(false);
+
+		Register.setSubtFlag(false);
+		Register.setHCarryFlag(false);
+		Register.setCarryFlag(false);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return how many cycles / 4 instruction took.
+		return 1;
+	}
+
+	public static int xor_$HL() //Opcode: 0xAE
+	{
+		int a = Memory.read(Register.readHL());
+		int b = Register.readA();
+		int temp = a ^ b;
+		Register.writeA(temp);
+
+		if(temp == 0) Register.setZeroFlag(true);
+		else Register.setZeroFlag(false);
+		Register.setSubtFlag(false);
+		Register.setHCarryFlag(false);
+		Register.setCarryFlag(false);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return how many cycles / 4 instruction took.
+		return 2;
+	}
+
+	public static int xor_n() //opcode: 0xEE
+	{
+		Register.incPC(1);
+
+		int a = Memory.read(Register.readPC());
+		int b = Register.readA();
+		int temp = a ^ b;
+		Register.writeA(temp);
+
+		if(temp == 0) Register.setZeroFlag(true);
+		else Register.setZeroFlag(false);
+		Register.setSubtFlag(false);
+		Register.setHCarryFlag(false);
+		Register.setCarryFlag(false);
+
+		//increase PC to next instruction
+		Register.incPC(1);
+
+		//return how many cycles / 4 instruction took.
+		return 2;
 	}
 
 	//Section 3.3.5 Miscellaneous
